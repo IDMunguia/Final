f456591eaceb042e3bc8d9be0d50a6df5d65af46
Diego Munguia <idiegomunguia@icloud.com>
2024-05-07 09:39:18 -0600

    boom

---
+++ Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+while (true)
+{
+    Console.WriteLine("Enter date:");
+    string inputDate = Console.ReadLine();
+    var todayDate = DateTime.Today;
+    if (DateTime.TryParse(inputDate, out DateTime dateOfBirth))
+    {
+        var age = todayDate - dateOfBirth;
+        Console.WriteLine($"You are {age} days old.");  
+    }
+    else
+    {
+        Console.WriteLine("Bad date. Please enter it MM/DD/YYYY format.");
+    }
+}
